9a8e56cdb1bbccc39240f39960470334c76491dc
Isaack Mungui <munguiisaack@gmail.com>
2022-09-07 15:03:38 +0300

    Added components and modules to main component

--- github-search-app/src/App.js
+++ github-search-app/src/App.js
@@ -1,7 +1,30 @@
+import Home from './components/Home'
+import Navbar from './components/Navbar'
+import Repo from './components/Repo'
+import User from './components/User'
+
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+
 function App() {
   return (
-    <div className="App">
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div className='content'>
+          <Switch>
+            <Route path='/'>
+              <Home />
+            </Route>
+            <Route path='/users'>
+              <User />
+            </Route>
+            <Route path='/repos'>
+              <Repo />
+            </Route>
+          </Switch>
+        </div>
+      </div>
+    </Router>
   );
 }
 
