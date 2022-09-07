47224120d920d34c3d3e695d8eea1fe380f8ae8f
Isaack Mungui <munguiisaack@gmail.com>
2022-09-07 15:21:40 +0300

    Added navigation links

---
+++ github-search-app/src/components/Navbar.js
@@ -0,0 +1,19 @@
+import { Link } from 'react-router-dom'
+
+const Navbar = () => {
+    return (
+        <div>
+            <li>
+                <Link to='/about'>About</Link>
+            </li>
+            <li>
+                <Link to='/users'>Users</Link>
+            </li>
+            <li>
+                <Link to='/repos'>Repos</Link>
+            </li>
+        </div>
+    )
+}
+
+export default Navbar
