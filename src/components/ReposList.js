ac15f2c47c82dc3226bd0b3fb7424f979c4e6373
Isaack Mungui <munguiisaack@gmail.com>
2022-10-05 13:20:59 +0300

    Received props from cparent nd passed  to child

--- src/components/ReposList.js
+++ src/components/ReposList.js
@@ -1,9 +1,37 @@
 import React from "react"
+import { useEffect, useState } from "react"
+import Form from "./Form"
 import Repo from "./Repo"
 
-function ReposList() {
+function ReposList({ repoInfo }) {
+    console.log(repoInfo);
+    // return (
+    //     <div>
+    //         <Form />
+    //         <Repo key={repos.id} name={repos.login} repo={repos} />
+    //         <table className="ui celled striped padded table">
+    //             <tbody>
+    //                 <tr>
+    //                 <th>
+    //                     <h3 className="ui center aligned header">Name</h3>
+    //                 </th>
+    //                 <th>
+    //                     <h3 className="ui center aligned header">Owner</h3>
+    //                 </th>
+    //                 <th>
+    //                     <h3 className="ui center aligned header">Description</h3>
+    //                 </th>
+    //                 <th>
+    //                     <h3 className="ui center aligned header">Deployed at</h3>
+    //                 </th>
+    //                 </tr>
+    //             </tbody>
+    //         </table>
+    //     </div>
+    // )
+
     return (
-        <Repo />
+        <Repo repo={repoInfo} />
     )
 }
 
