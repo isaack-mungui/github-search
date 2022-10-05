85961c64160b6244494ce577e23191bad687039c
Isaack Mungui <munguiisaack@gmail.com>
2022-10-05 13:18:53 +0300

    Executed fetch API on multiple endpoints and passed props

--- src/components/SearchContainer.js
+++ src/components/SearchContainer.js
@@ -1,13 +1,15 @@
 import React from "react"
 import { useEffect, useState } from "react"
 import NavBar from "./NavBar"
+import ReposList from "./ReposList"
 import UsersList from "./UsersList"
 
 function SearchContainer() {
     const [userData, setUserData] = useState([])
+    const [repos, setRepos] = useState([])
 
     useEffect(() => {
-        fetch("https://api.github.com/users/isaack-mungui/repos")
+        fetch("https://api.github.com/search/users?q=type:user")
         .then((r) => r.json())
         .then((data) => {
             console.log(data);
@@ -15,10 +17,22 @@ function SearchContainer() {
         })
     }, [])
 
-    return (
+
+    useEffect(() => {
+        fetch("https://api.github.com/users/jumaallan/repos")
+        .then((r) => r.json())
+        .then((data) => {
+            console.log(data);
+            setRepos(data)
+        })
+    }, [])
+
+
+    return (    
         <div>
             <NavBar />
-            <UsersList data={userData} />
+            <ReposList repoInfo={repos} />
+            <UsersList userInfo={userData} />
         </div>
     )
 }
