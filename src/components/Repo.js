a75c2e26af7ae66ef122763420be370ed5b54eb5
Isaack Mungui <munguiisaack@gmail.com>
2022-10-05 14:22:10 +0300

    Added card to render repository data

--- src/components/Repo.js
+++ src/components/Repo.js
@@ -1,10 +1,14 @@
-function Repo({ repo }) {
-    console.log(repo);
-    // return (
-    //     <tr>
-    //         <td>{name}</td>
-    //     </tr>
-    // )
+function Repo({ topics }) {
+    return (
+        <div className="card">
+            <div className="card-body">
+                <h4 className="card-title">Card title</h4>
+                <p className="card-text">Some example text. Some example text.</p>
+                <a href="#" className="card-link">Card link: {topics}</a>
+                <a href="#" className="card-link">Another link</a>
+            </div>
+        </div>
+    )
 }
 
 export default Repo
