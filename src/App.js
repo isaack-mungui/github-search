7f4c272dfc11f02c22c6e12708ea82305d76d4ef
Isaack Mungui <munguiisaack@gmail.com>
2022-10-05 14:23:58 +0300

    Rendered Repo component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import SearchContainer from './components/SearchContainer';
+import Repo from './components/Repo';
 
 function App() {
   return (
       <div className="App">
         <SearchContainer />
+        <Repo />
       </div>
   );
 }
