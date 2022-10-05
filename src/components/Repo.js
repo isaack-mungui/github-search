f06cab565f2e42927e1e1eb64dc26bef262c3db6
Isaack Mungui <munguiisaack@gmail.com>
2022-10-05 13:19:54 +0300

    Received props from its parent

--- src/components/Repo.js
+++ src/components/Repo.js
@@ -1,5 +1,10 @@
-function Repo() {
-
+function Repo({ repo }) {
+    console.log(repo);
+    // return (
+    //     <tr>
+    //         <td>{name}</td>
+    //     </tr>
+    // )
 }
 
 export default Repo
