32bef29dda92e75b8b9a159848809ddba7c06690
Isaack Mungui <munguiisaack@gmail.com>
2022-10-05 13:22:32 +0300

    Received props from parent

--- src/components/User.js
+++ src/components/User.js
@@ -1,4 +1,19 @@
-function User() {
+function User({ users }) {
+    console.log(users);
+    // return (
+    //     // <div class="card">
+    //     //     <div class="card-body">
+    //     //         <h4 class="card-title">Card title</h4>
+    //     //         <p class="card-text">Some example text. Some example text.</p>
+    //     //         <a href="#" class="card-link">Card link</a>
+    //     //         <a href="#" class="card-link">Another link</a>
+    //     //     </div>
+    //     // </div>
+
+    //     // {users.map((user) => (
+    //     //     <
+    //     // ))
+    // ) 
 
 }
 
