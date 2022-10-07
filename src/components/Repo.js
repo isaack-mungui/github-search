f0b50b6df982a05060e349392546116f86e8a538
Isaack Mungui <munguiisaack@gmail.com>
2022-10-07 12:15:36 +0300

    Received props from parent and rendered to card

--- src/components/Repo.js
+++ src/components/Repo.js
@@ -1,11 +1,14 @@
-function Repo({ topics }) {
+function Repo({ name, desc, languages, topics, url }) {
     return (
         <div className="card">
             <div className="card-body">
-                <h4 className="card-title">Card title</h4>
-                <p className="card-text">Some example text. Some example text.</p>
-                <a href="#" className="card-link">Card link: {topics}</a>
-                <a href="#" className="card-link">Another link</a>
+                <h4 className="card-title">{name}</h4>
+                <p className="card-text">{desc}</p>
+                <a href="#" className="card-link">Built using: {languages}</a>
+                <br/>
+                <a href="#" className="card-link">{topics}</a>
+                <br/>
+                <a href={url} className="card-link">Visit Repo</a>
             </div>
         </div>
     )
