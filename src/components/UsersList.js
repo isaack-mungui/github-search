68dc03adecfc8d07834b53b53bd4e143cb1e03dd
Isaack Mungui <munguiisaack@gmail.com>
2022-10-07 12:19:42 +0300

    Added side effect and passed props to child

--- src/components/UsersList.js
+++ src/components/UsersList.js
@@ -1,14 +1,25 @@
 import React from "react"
-// import Card from "./Card";
-import { useEffect, useState } from 'react'
 import User from "./User"
+import { useEffect, useState } from "react"
+import SearchBar from "./SearchBar"
 
-function UsersList({ userInfo }) {
-    console.log(userInfo);
+function UsersList() {
+    const [users, setUsers] = useState([])
+    
+    useEffect(() => {
+        fetch("https://api.github.com/search/users?q=type:user")
+        .then((r) => r.json())
+        .then((data) => {
+            setUsers(data.items)
+        })
+    }, [])
+
     return (
         <div>
-            {/* <Card /> */}
-            <User users={userInfo} />
+            <SearchBar />
+            {users.map((user) => (
+                <User key={user.id} url={user.avatar_url} login={user.login} repos={user.repos_url} />
+            ))}
         </div>  
     )
 }
