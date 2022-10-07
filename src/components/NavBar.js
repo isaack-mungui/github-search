a181758f6313a724712412810010da3712bd214b
Isaack Mungui <munguiisaack@gmail.com>
2022-10-07 12:14:34 +0300

    Added links and styling to navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,26 +1,25 @@
+import React from "react"
+import { NavLink } from "react-router-dom"
+
+
+const linkStyles = {
+    display: "inline-block",
+    width: "50px",
+    padding: "12px",
+    margin: "0 6px 6px",
+    textDecoration: "none",
+    color: "black",
+};
+
 function NavBar() {
     return (
-        <nav className="navbar navbar-expand-lg bg-light">
-            <div className="container-fluid">
-                <a className="navbar-brand" href="#">GitHub Search</a>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li className="nav-item">
-                        <a className="nav-link active" aria-current="page" href="#">Home</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link" href="#">Users</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link" href="#">Repositories</a>
-                    </li>
-                </ul>
-                </div>
+        <div>
+            <div>
+                <NavLink to="/" style={linkStyles}>Home</NavLink>
+                <NavLink to="/user" style={linkStyles}>Users</NavLink>
+                <NavLink to="/repo" style={linkStyles}>Repositories</NavLink>
             </div>
-        </nav>
+        </div>
     )
 }
 
