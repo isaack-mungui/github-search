8f5f932fe264aee412d8479b20d0251e3c502db5
Isaack Mungui <munguiisaack@gmail.com>
2022-10-07 12:12:37 +0300

    Removed repo component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import './App.css';
 import SearchContainer from './components/SearchContainer';
-import Repo from './components/Repo';
 
 function App() {
   return (
       <div className="App">
         <SearchContainer />
-        <Repo />
       </div>
   );
 }
